1eb55614c6b71aa6ec94b67184f4d88e9a5275b5
Kanan Farzaliyev <kanan.farzali@gmail.com>
2026-01-07 10:30:33 +0100

    complete initial setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
-  return <div>Home</div>
+const Home = () => {
+  return (
+    <p className="text-3xl font-bold text-indigo-500 underline">CoinStream</p>
+  )
 }
+
+export default Home
